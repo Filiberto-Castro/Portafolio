c1b3152a6426c0db734ef95c63cdc71d404d0bd0
Filiberto_Castro <fcastrob@miumg.edu.gt>
2023-08-17 11:23:25 -0600

    Mejoramiento del diseño y funcionamiento

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
+    navbar.classList.toggle('active');
 }
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x')
+    navbar.classList.remove('active')
+};
