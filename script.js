08f13084badeb1933db8509421860920bb3114cf
Filiberto-Castro <fcastrob@miumg.edu.gt>
2024-08-02 18:57:56 -0600

    Actualizacion - Cart agregado, disenio mejorado

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+// Selección de elementos del menú y secciones
+let menuItems = document.querySelectorAll("ul li a");
+let sections = document.querySelectorAll("section");
 
+// Menu Bar para movil
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
 window.onscroll = () => {
-    menu.classList.remove('bx-x')
-    navbar.classList.remove('active')
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+// section active en menubar
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      menuItems.forEach((link) => {
+        link.classList.remove("active");
+        if (link.getAttribute("href").includes(id)) {
+          link.classList.add("active");
+        }
+      });
+    }
+  });
 };
